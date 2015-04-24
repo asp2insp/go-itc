0eaa833ee00f5ac04fb713beea07f1db3883bf86
Josiah Gaskin <josiahgaskin@gmail.com>
2015-04-23 23:13:35 -0700

    Working on normalizing events

--- ITC/event_ops.go
+++ ITC/event_ops.go
@@ -1,1 +1,45 @@
 package ITC
+
+func lift(e *Event, m int) *Event {
+	e.n += m
+	return e
+}
+
+func sink(e *Event, m int) *Event {
+	e.n -= m
+	if isLeaf(e) && e.n == 0 {
+		return nil
+	} else {
+		return e
+	}
+}
+
+func normEvent(e *Event) *Event {
+	switch {
+	case isLeaf(e): // norm(n) => n
+		return e
+	case isLeaf(e.el) && isLeaf(e.er) && e.el.n == e.er.n:
+		e = lift(e, e.el.n)
+		e.el = nil
+		e.er = nil
+		return e
+	default:
+		m := min(e.el, e.er)
+		lift(e, m)
+		sink(e.el, m)
+		sink(e.er, m)
+		return e
+	}
+}
+
+func min(e1, e2 *Event) int {
+	if e1.n < e2.n {
+		return e1.n
+	} else {
+		return e2.n
+	}
+}
+
+func isLeaf(e *Event) bool {
+	return e.el == nil && e.er == nil
+}
--- ITC/event_ops_test.go
+++ ITC/event_ops_test.go
@@ -1,1 +1,62 @@
 package ITC
+
+import (
+	"testing"
+
+	"github.com/ca-geo/go-misc/testutils"
+)
+
+// ================= NORM =======================
+
+// normEvent(n) => n
+func TestNormEventAtom(t *testing.T) {
+	e := stringToEvent("1")
+	normed := normEvent(e)
+	testutils.CheckString("1", normed.String(), t)
+}
+
+// normEvent((3, 1, 1)) => 4
+func TestNormEventTreeToAtom(t *testing.T) {
+	e := stringToEvent("(3,1,1)")
+	normed := normEvent(e)
+	testutils.CheckString("4", normed.String(), t)
+}
+
+// normEvent((3, 1, 2)) => (4, 0, 1)
+func TestNormEventTreeWithMismatchedLeaf(t *testing.T) {
+	e := stringToEvent("(3,1,2)")
+	normed := normEvent(e)
+	testutils.CheckString("(4, 0, 1)", normed.String(), t)
+}
+
+// ================= LIFT =======================
+
+// lift(n, m) => n+m
+func TestLiftAtom(t *testing.T) {
+	e := stringToEvent("1")
+	eLifted := lift(e, 3)
+	testutils.CheckString("4", eLifted.String(), t)
+}
+
+// lift((n, el, er), m) => (n+m, el, er)
+func TestLiftRoot(t *testing.T) {
+	e := stringToEvent("(1, 4, 7)")
+	eLifted := lift(e, 3)
+	testutils.CheckString("(4, 4, 7)", eLifted.String(), t)
+}
+
+// ================= SINK =======================
+
+// sink(n, m) => n-m
+func TestSinkAtom(t *testing.T) {
+	e := stringToEvent("5")
+	eSunk := sink(e, 3)
+	testutils.CheckString("2", eSunk.String(), t)
+}
+
+// sink((n, el, er), m) => (n-m, el, er)
+func TestSinkRoot(t *testing.T) {
+	e := stringToEvent("(8, 4, 7)")
+	eSunk := sink(e, 3)
+	testutils.CheckString("(5, 4, 7)", eSunk.String(), t)
+}
